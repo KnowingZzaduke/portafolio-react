b127e896b6f79fe42b8a52536569f0316a645088
KnowingZzaduke <josex.developer@gmail.com>
2023-03-29 11:25:49 -0500

    Agregando el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,47 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
+import Swal from "sweetalert2";
 export function Header() {
-  const [time, setTime] = useState(new Date())
-    const [location, setLocation] = useState({})
+  const [time, setTime] = useState(new Date());
+  const [location, setLocation] = useState({});
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime(new Date())
-    }, 1000)
-  }, [])
+      setTime(new Date());
+    }, 1000);
+  }, []);
 
   useEffect(() => {
     const getLocation = async () => {
-      if (navigator.geolocation) {
-        try {
-          const position = await new Promise((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition(resolve, reject);
-          });
-          const { latitude, longitude } = position.coords;
-          const response = await fetch(
-            `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=es`
-          );
-          const data = await response.json();
-          setLocation(data);
-        } catch (error) {
-          console.error(error);
-        }
+      const { state } = await navigator.permissions.query({
+        name: "geolocation",
+      });
+      if (state === "denied") {
+        Swal.fire({
+          title: "Denegado",
+          text: "Se ha denegado el acceso a tu ubicación 😕",
+          icon: "warning",
+        });
       } else {
-        console.log("Geolocation is not supported by this browser.");
+        if (navigator.geolocation) {
+          try {
+            const position = await new Promise((resolve, reject) => {
+              navigator.geolocation.getCurrentPosition(resolve, reject);
+            });
+            const { latitude, longitude } = position.coords;
+            const response = await fetch(
+              `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=es`
+            );
+            const data = await response.json();
+            setLocation(data);
+          } catch (error) {
+            console.error(error);
+          }
+        } else {
+          Swal.fire({
+            title: "Error",
+            text: "Geolocation no es soportado por tu navegador",
+            icon: "error",
+          });
+        }
       }
     };
 
@@ -35,15 +50,32 @@ export function Header() {
 
   return (
     <div className="content_header">
-      <p>
-        <span>{time.toLocaleTimeString()}</span>
-        <span>-</span>
-        <span>{location.countryName}</span>
-        <span>{location.city}</span>
-        <span>.</span>
-        <span>Pais</span>
-        <span>Ciudad</span>
-      </p>
+      <div className="content_data">
+        <p>
+          <span>{time.toLocaleTimeString()}</span>
+          <span>𝆺</span>
+          <span>Colombia</span>
+          <span>-</span>
+          <span>Cartagena</span>
+          <div className="content_data-user">
+            <span>ϟ</span>
+            <span>{location.countryName}</span>
+            <span>-</span>
+            <span>{location.city}</span>
+          </div>
+        </p>
+      </div>
+      <div className="content_description">
+        <h2>Sobre mi</h2>
+        <p>
+          Saludos👋 estimados, mi nombre es Jose Luis Arteta Buelvas, soy un
+          desarrollador Junior Front-end autodidacta utilizando las tecnologías
+          Html, Css, Sass, JavaScript, React, Github y la herramienta de diseño
+          Figma. Me apasiona mucho aprender cosas nuevas cada día que aporten
+          gran valor a mi vida y al mundo. Me interesa todo lo relacionado con
+          la tecnología y la programación.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
