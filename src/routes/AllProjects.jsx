4f0da76925f2263bf6d5da3200e0d8d8f50f53dd
KnowingZzaduke <josex.developer@gmail.com>
2023-03-30 20:58:47 -0500

    Agregando proyectos

--- src/routes/AllProjects.jsx
+++ src/routes/AllProjects.jsx
@@ -1,15 +1,53 @@
 import { Link } from "react-router-dom";
 import JoseLogo from "/video/jose-logo-black.mp4";
-import { useState, useEffect } from "react";
+import Whatssive from "/video/Whatssive.mp4";
+import Taks from '/video/taks.mp4'
+import Carrito from "/video/carrito.mp4";
+import Reloj from "/video/reloj.mp4";
+import Calculadora from "/video/calculadora-JS.mp4";
+import Contador from "/video/contador-JS.mp4";
+
+import Dysam from "/img/Dysam.jpg";
+import { useState } from "react";
 export function AllProjects() {
-  const [click, setClick] = useState(false);
-  function handleClick() {
-    setClick(!click);
+  const [portafolio, setPortafolio] = useState(false);
+  const [dysam, setDysam] = useState(false);
+  const [whatssive, setWhatssive] = useState(false);
+  const [taks, setTaks] = useState(false);
+  const [carrito, setCarrito] = useState(false);
+  const [reloj, setReloj] = useState(false);
+  const [calculadora, setCalculadora] = useState(false);
+  const [contador, setContador] = useState(false);
+
+  function handlePortafolio() {
+    setPortafolio(!portafolio);
   }
+  function handleDysam() {
+    setDysam(!dysam);
+  }
+  function handleWhatssive() {
+    setWhatssive(!whatssive);
+  }
+  function handleTaks() {
+    setTaks(!taks);
+  }
+  function handleCarrito() {
+    setCarrito(!carrito);
+  }
+  function handleReloj() {
+    setReloj(!reloj);
+  }
+  function handleCalculadora() {
+    setCalculadora(!calculadora);
+  }
+  function handleContador() {
+    setContador(!contador);
+  }
+
   return (
     <div className="content_all-projects">
       <h2>Proyectos</h2>
-      <Link onClick={handleClick}>
+      <Link onClick={handlePortafolio}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Portafolio</h3>
@@ -21,7 +59,7 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              portafolio ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
@@ -31,11 +69,11 @@ export function AllProjects() {
                 loop
                 muted
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  portafolio ? "video_whatssive-d" : ""
                 }`}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div className={`info_right-n ${portafolio ? "info_right-d" : ""}`}>
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
                 <h3>Portafolio</h3>
@@ -58,7 +96,7 @@ export function AllProjects() {
           </div>
         </div>
       </Link>
-      <Link to="/proyectos/reportes-dysam">
+      <Link onClick={handleDysam}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Dysam reportes</h3>
@@ -70,24 +108,22 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              dysam ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
-              <video
-                src={JoseLogo}
-                autoPlay
-                loop
-                muted
+              <img
+                src={Dysam}
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  dysam ? "video_whatssive-d" : ""
                 }`}
+                style={{height: "300px"}}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div className={`info_right-n ${dysam ? "info_right-d" : ""}`}>
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
-                <h3>Portafolio</h3>
+                <h3>Reportes Dysam</h3>
               </div>
               <div className="info">
                 <div className="stack">
@@ -107,7 +143,7 @@ export function AllProjects() {
           </div>
         </div>
       </Link>
-      <Link>
+      <Link onClick={handleWhatssive}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Whatssive</h3>
@@ -117,9 +153,46 @@ export function AllProjects() {
             pdf con datos específicos y el módulo de usuarios permite la
             consulta de estos mediante un código generado
           </p>
+          <div
+            className={`content_info-project-n ${
+              whatssive ? "content_info-project-d" : ""
+            }`}
+          >
+            <div className="info_left">
+              <video
+                src={Whatssive}
+                autoPlay
+                loop
+                muted
+                className={`video_whatssive-n ${
+                  whatssive ? "video_whatssive-d" : ""
+                }`}
+              />
+            </div>
+            <div className={`info_right-n ${whatssive ? "info_right-d" : ""}`}>
+              <div className="date_info">
+                <span>Marzo 22, 2023</span>
+                <h3>Whatssive</h3>
+              </div>
+              <div className="info">
+                <div className="stack">
+                  <h4>Stack</h4>
+                  <p>React, Sass</p>
+                </div>
+                <div className="platform">
+                  <h4>Plataforma</h4>
+                  <p>Web</p>
+                </div>
+                <div className="Website">
+                  <h4>Dirección web</h4>
+                  <p>Dirección web</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </Link>
-      <Link>
+      <Link onClick={handleTaks}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Taks list</h3>
@@ -131,24 +204,24 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              taks ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
               <video
-                src={JoseLogo}
+                src={Taks}
                 autoPlay
                 loop
                 muted
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  taks ? "video_whatssive-d" : ""
                 }`}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div className={`info_right-n ${taks ? "info_right-d" : ""}`}>
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
-                <h3>Portafolio</h3>
+                <h3>Taks List</h3>
               </div>
               <div className="info">
                 <div className="stack">
@@ -168,7 +241,7 @@ export function AllProjects() {
           </div>
         </div>
       </Link>
-      <Link>
+      <Link onClick={handleCarrito}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Carrito de compras</h3>
@@ -180,24 +253,24 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              carrito ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
               <video
-                src={JoseLogo}
+                src={Carrito}
                 autoPlay
                 loop
                 muted
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  carrito ? "video_whatssive-d" : ""
                 }`}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div className={`info_right-n ${carrito ? "info_right-d" : ""}`}>
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
-                <h3>Portafolio</h3>
+                <h3>Carrito de compras</h3>
               </div>
               <div className="info">
                 <div className="stack">
@@ -217,7 +290,7 @@ export function AllProjects() {
           </div>
         </div>
       </Link>
-      <Link>
+      <Link onClick={handleReloj}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Reloj</h3>
@@ -229,24 +302,24 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              reloj ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
               <video
-                src={JoseLogo}
+                src={Reloj}
                 autoPlay
                 loop
                 muted
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  reloj ? "video_whatssive-d" : ""
                 }`}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div className={`info_right-n ${reloj ? "info_right-d" : ""}`}>
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
-                <h3>Portafolio</h3>
+                <h3>Reloj</h3>
               </div>
               <div className="info">
                 <div className="stack">
@@ -266,7 +339,7 @@ export function AllProjects() {
           </div>
         </div>
       </Link>
-      <Link>
+      <Link onClick={handleCalculadora}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Calculadora</h3>
@@ -278,24 +351,26 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              calculadora ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
               <video
-                src={JoseLogo}
+                src={Calculadora}
                 autoPlay
                 loop
                 muted
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  calculadora ? "video_whatssive-d" : ""
                 }`}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div
+              className={`info_right-n ${calculadora ? "info_right-d" : ""}`}
+            >
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
-                <h3>Portafolio</h3>
+                <h3>Calculadora</h3>
               </div>
               <div className="info">
                 <div className="stack">
@@ -315,7 +390,7 @@ export function AllProjects() {
           </div>
         </div>
       </Link>
-      <Link>
+      <Link onClick={handleContador}>
         <div className="proyecto">
           <span>Marzo 22, 2023</span>
           <h3>Contador</h3>
@@ -327,24 +402,24 @@ export function AllProjects() {
           </p>
           <div
             className={`content_info-project-n ${
-              click ? "content_info-project-d" : ""
+              contador ? "content_info-project-d" : ""
             }`}
           >
             <div className="info_left">
               <video
-                src={JoseLogo}
+                src={Contador}
                 autoPlay
                 loop
                 muted
                 className={`video_whatssive-n ${
-                  click ? "video_whatssive-d" : ""
+                  contador ? "video_whatssive-d" : ""
                 }`}
               />
             </div>
-            <div className={`info_right-n ${click ? "info_right-d" : ""}`}>
+            <div className={`info_right-n ${contador ? "info_right-d" : ""}`}>
               <div className="date_info">
                 <span>Marzo 22, 2023</span>
-                <h3>Portafolio</h3>
+                <h3>Contador</h3>
               </div>
               <div className="info">
                 <div className="stack">
